d8b716061760eb8c440bdf191d23901f24b8484a
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-05-18 19:22:19 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Example from './js/template.js';
+
+
+$(document).ready(function() {
+  // ui logic
+});
